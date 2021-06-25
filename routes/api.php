78f512dcb77eb047fe6c70fc7c0b66dceff3f647
prom12 <51293420+Prom12@users.noreply.github.com>
2021-06-25 09:00:32 +0000

    api,web
    auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Model\User\courses;
+use App\Model\User\categories;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/',function(){
+    return courses::all();
+});
+
+
+Route::get('/courses',function(){
+    return categories::all();
+})->name('courses');
